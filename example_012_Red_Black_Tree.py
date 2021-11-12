a8004dadbc883dad4f7f75a6d150927bd8fece2b
chhunhorn <56245196+chhunhorn@users.noreply.github.com>
2021-11-11 22:12:50 -0700

    dsd

---
+++ example_012_Red_Black_Tree.py
@@ -0,0 +1,154 @@
+import random
+
+
+class RBNode:
+    def __init__(self, val):
+        self.red = False
+        self.parent = None
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class RBTree:
+    def __init__(self):
+        self.nil = RBNode(0)
+        self.nil.red = False
+        self.nil.left = None
+        self.nil.right = None
+        self.root = self.nil
+
+    def insert(self, val):
+        # Ordinary Binary Search Insertion
+        new_node = RBNode(val)
+        new_node.parent = None
+        new_node.left = self.nil
+        new_node.right = self.nil
+        new_node.red = True  # new node must be red
+
+        parent = None
+        current = self.root
+        while current != self.nil:
+            parent = current
+            if new_node.val < current.val:
+                current = current.left
+            elif new_node.val > current.val:
+                current = current.right
+            else:
+                return
+
+        # Set the parent and insert the new node
+        new_node.parent = parent
+        if parent == None:
+            self.root = new_node
+        elif new_node.val < parent.val:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+        # Fix the tree
+        self.fix_insert(new_node)
+
+    def fix_insert(self, new_node):
+        while new_node != self.root and new_node.parent.red:
+            if new_node.parent == new_node.parent.parent.right:
+                u = new_node.parent.parent.left  # uncle
+                if u.red:
+                    u.red = False
+                    new_node.parent.red = False
+                    new_node.parent.parent.red = True
+                    new_node = new_node.parent.parent
+                else:
+                    if new_node == new_node.parent.left:
+                        new_node = new_node.parent
+                        self.rotate_right(new_node)
+                    new_node.parent.red = False
+                    new_node.parent.parent.red = True
+                    self.rotate_left(new_node.parent.parent)
+            else:
+                u = new_node.parent.parent.right  # uncle
+
+                if u.red:
+                    u.red = False
+                    new_node.parent.red = False
+                    new_node.parent.parent.red = True
+                    new_node = new_node.parent.parent
+                else:
+                    if new_node == new_node.parent.right:
+                        new_node = new_node.parent
+                        self.rotate_left(new_node)
+                    new_node.parent.red = False
+                    new_node.parent.parent.red = True
+                    self.rotate_right(new_node.parent.parent)
+        self.root.red = False
+
+    def exists(self, val):
+        curr = self.root
+        while curr != self.nil and val != curr.val:
+            if val < curr.val:
+                curr = curr.left
+            else:
+                curr = curr.right
+        return curr
+
+    # rotate left at node x
+    def rotate_left(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.nil:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    # rotate right at node x
+    def rotate_right(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.nil:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def __repr__(self):
+        lines = []
+        print_tree(self.root, lines)
+        return '\n'.join(lines)
+
+
+ 
+
+def print_tree(node, lines, level = 0):
+    if node != None:
+        print_tree(node.left, lines, level + 1)
+        print(' ' * 4 * level + '->', node.val,
+              ' ' + ('r' if node.red else 'b'))
+        print_tree(node.right, lines, level + 1)
+
+
+
+def main():
+    tree = RBTree()
+    for x in range(1, 30):
+        tree.insert(x)
+    print(tree)
+
+
+main()
+
+
---
+++ example_09-1-Graph.py
@@ -0,0 +1,200 @@
+
+import sys
+
+class Stack (object):
+  def __init__ (self):
+    self.stack = []
+
+  # add an item to the top of the stack
+  def push (self, item):
+    self.stack.append (item)
+
+  # remove an item from the top of the stack
+  def pop (self):
+    return self.stack.pop()
+
+  # check the item on the top of the stack
+  def peek (self):
+    return self.stack[-1]
+
+  # check if the stack if empty
+  def is_empty (self):
+    return (len (self.stack) == 0)
+
+  # return the number of elements in the stack
+  def size (self):
+    return (len (self.stack))
+
+
+class Queue (object):
+  def __init__ (self):
+    self.queue = []
+
+  # add an item to the end of the queue
+  def enqueue (self, item):
+    self.queue.append (item)
+
+  # remove an item from the beginning of the queue
+  def dequeue (self):
+    return (self.queue.pop(0))
+
+  # check if the queue is empty
+  def is_empty (self):
+    return (len (self.queue) == 0)
+
+  # return the size of the queue
+  def size (self):
+    return (len (self.queue))
+
+
+class Vertex (object):
+  def __init__ (self, label):
+    self.label = label
+    self.visited = False
+
+  # determine if a vertex was visited
+  def was_visited (self):
+    return self.visited
+
+  # determine the label of the vertex
+  def get_label (self):
+    return self.label
+
+  # string representation of the vertex
+  def __str__ (self):
+    return str (self.label)
+
+
+class Graph (object):
+  def __init__ (self):
+    self.Vertices = []
+    self.adjMat = []
+
+  # check if a vertex is already in the graph
+  def has_vertex (self, label):
+    nVert = len (self.Vertices)
+    for i in range (nVert):
+      if (label == (self.Vertices[i]).get_label()):
+        return True
+    return False
+
+  # given the label get the index of a vertex
+  def get_index (self, label):
+    nVert = len (self.Vertices)
+    for i in range (nVert):
+      if (label == (self.Vertices[i]).get_label()):
+        return i
+    return -1
+
+  # add a Vertex with a given label to the graph
+  def add_vertex (self, label):
+    if (self.has_vertex (label)):
+      return
+
+    # add vertex to the list of vertices
+    self.Vertices.append (Vertex (label))
+
+    # add a new column in the adjacency matrix
+    nVert = len (self.Vertices)
+    for i in range (nVert - 1):
+      (self.adjMat[i]).append (0)
+
+    # add a new row for the new vertex
+    new_row = []
+    for i in range (nVert):
+      new_row.append (0)
+    self.adjMat.append (new_row)
+
+  # add weighted directed edge to graph
+  def add_directed_edge (self, start, finish, weight = 1):
+    self.adjMat[start][finish] = weight
+
+  # add weighted undirected edge to graph
+  def add_undirected_edge (self, start, finish, weight = 1):
+    self.adjMat[start][finish] = weight
+    self.adjMat[finish][start] = weight
+
+  # return an unvisited vertex adjacent to vertex v (index)
+  def get_adj_unvisited_vertex (self, v):
+    nVert = len (self.Vertices)
+    for i in range (nVert):
+      if (self.adjMat[v][i] > 0) and (not (self.Vertices[i]).was_visited()):
+        return i
+    return -1
+
+  # do a depth first search in a graph
+  def dfs (self, v):
+    # create the Stack
+    theStack = Stack ()
+
+    # mark the vertex v as visited and push it on the Stack
+    (self.Vertices[v]).visited = True
+    print (self.Vertices[v])
+    theStack.push (v)
+
+    # visit all the other vertices according to depth
+    while (not theStack.is_empty()):
+      # get an adjacent unvisited vertex
+      u = self.get_adj_unvisited_vertex (theStack.peek())
+      if (u == -1):
+        u = theStack.pop()
+      else:
+        (self.Vertices[u]).visited = True
+        print (self.Vertices[u])
+        theStack.push (u)
+
+    # the stack is empty, let us rest the flags
+    nVert = len (self.Vertices)
+    for i in range (nVert):
+      (self.Vertices[i]).visited = False
+
+  # do the breadth first search in a graph
+  def bfs (self, v):
+    return
+
+def main():
+  # create the Graph object
+  cities = Graph()
+
+  # read the number of vertices
+  line = sys.stdin.readline()
+  line = line.strip()
+  num_vertices = int (line)
+
+  # read the vertices to the list of Vertices
+  for i in range (num_vertices):
+    line = sys.stdin.readline()
+    city = line.strip()
+    cities.add_vertex (city)
+
+  # read the number of edges
+  line = sys.stdin.readline()
+  line = line.strip()
+  num_edges = int (line)
+
+  # read each edge and place it in the adjacency matrix
+  for i in range (num_edges):
+    line = sys.stdin.readline()
+    edge = line.strip()
+    edge = edge.split()
+    start = int (edge[0])
+    finish = int (edge[1])
+    weight = int (edge[2])
+
+    cities.add_directed_edge (start, finish, weight)
+
+  # read the starting vertex for dfs and bfs
+  line = sys.stdin.readline()
+  start_vertex = line.strip()
+
+  # get the index of the starting vertex
+  start_index = cities.get_index (start_vertex)
+
+  # do the depth first search
+  print ("Depth First Search")
+  cities.dfs (start_index)
+  print ()
+    
+if __name__ == "__main__":
+  main()
+
