465bb5ef98430312752463e6b3d22d42b819f79d
kiat <kiat@cs.utexas.edu>
2022-02-08 00:09:38 -0600

    update

--- example_002.01_multiple_inheritanc.py
+++ example_002.01_multiple_inheritanc.py
@@ -14,4 +14,4 @@ x = C()
 x.do_it()
 x.do_it(2)
 # Multiple inheritance with old-style classes is governed by two rules: depth-first and then left-to-right.
-
+# more info here https://www.geeksforgeeks.org/multiple-inheritance-in-python/
