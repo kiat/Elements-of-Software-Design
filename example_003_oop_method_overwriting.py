c2921861b4682020b5909c09268a943486cd1bd0
kiat <kiat@cs.utexas.edu>
2022-06-15 23:11:43 -0500

    updated example 003

--- example_003_oop_method_overwriting.py
+++ example_003_oop_method_overwriting.py
@@ -11,7 +11,7 @@ class Driver:
         return "I am " + self.name + " with salary of " + str(self.salary)
 
     def work_per_day(self, salary_per_hour):
-        return self.salary/(30*salary_per_hour)
+        return round(self.salary/(30*salary_per_hour), 2)
 
 
 
@@ -33,14 +33,14 @@ class StudentDriver(Driver, Student):
 
     def __init__(self, name, salary, courses):
         super().__init__(name, salary)
-        Student.__init__(self, salary)
+        Student.__init__(self, name, courses)
  
         
     def __str__(self):
         return "I am " + self.name + " with numer of courses " + str(self.courses) + " , and with salary of " + str(self.salary)
 
     def work_per_day(self):
-        return super().work_per_day(15) + super().work_per_day(self)
+        return super().work_per_day(15) + Student.work_per_day(self)
 
 
 
@@ -52,7 +52,7 @@ def main():
     chris = StudentDriver("Chris", 2000, 4)
 
     print(chris)
-    print("Chris works "+str(chris.work_per_day())+" each single day.")
+    print("Chris works "+str(round(chris.work_per_day(), 2))+" hours each single day.")
 
 
 if __name__ == "__main__":
