967eab3200459e1982c508973cc808ddeec7fe57
kiat <kiat@cs.utexas.edu>
2023-02-23 07:48:42 -0600

    update

--- example_017_2_matrix_multiplication_strassen.py
+++ example_017_2_matrix_multiplication_strassen.py
@@ -8,6 +8,35 @@ def split(matrix):
 	row2, col2 = row//2, col//2
 	return matrix[:row2, :col2], matrix[:row2, col2:], matrix[row2:, :col2], matrix[row2:, col2:]
 
+
+def multi(a, b):
+	'''
+	Computes matrix product by divide and conquer approach, recursively.
+	Input: n x n numpy array matrices a and b
+	Output: n x n matrix, product of a and b
+	'''
+
+	# Base case when size of matrices is 1x1
+	if len(a) == 1:
+		return a * b
+
+	# Splitting the matrices into quadrants. This will be done recursively
+	# until the base case is reached.
+	a11, a12, a21, a22 = split(a)
+	b11, b12, b21, b22 = split(b)
+
+	# Computing the values of the 4 quadrants of the final matrix c
+	c11 = multi(a11, b11) + multi(a12, b21)
+	c12 = multi(a11, b12) + multi(a12, b22)
+	c21 = multi(a21, b11) + multi(a22, b21)
+	c22 = multi(a21, b12) + multi(a22, b22)
+
+	# Combining the 4 quadrants into a single matrix by stacking horizontally and vertically.
+	c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+
+	return c
+
+
 def strassen(a, b):
 	'''
 	Computes matrix product by divide and conquer approach, recursively.
@@ -58,5 +87,7 @@ if __name__ == "__main__":
 
 	print("Matrix A:\n", matrix_A)
 	print("Matrix B:\n", matrix_B)
+
+	print("Matrix C=:\n", multi(matrix_A, matrix_B))
 
 	print("Matrix C=:\n", strassen(matrix_A, matrix_B))
