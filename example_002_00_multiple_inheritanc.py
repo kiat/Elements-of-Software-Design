682c4318fd8db7908f7bd495514deb110fe35e36
Kia Teymourian <kiat@cs.utexas.edu>
2023-08-04 15:41:01 -0500

    update formating

--- example_002_00_multiple_inheritanc.py
+++ example_002_00_multiple_inheritanc.py
@@ -1,36 +1,48 @@
 """ Multiple Inheritance Example in Python 3. """
+import math
 
-class MY_A:
+class MyA:
     """A simple example class A"""
+    def __init__(self, my_x, my_y):
+        self.my_x = my_x
+        self.my_y = my_y
+
+    def calculate(self):
+        """A simple root square calculation."""
+        return math.sqrt(self.my_x**2 + self.my_y**2)
 
     def do_it(self):
+        """A simple method do demonstrate the call of this function. """
         print("method of Class A is called")
+        print(self.calculate())
 
-class MY_B(MY_A):
+class MyB(MyA):
     "A class B that inherits Class A. "
     # pass
 
 
-class MY_C(MY_A):
+class MyC(MyA):
     "A class C that also inherits Class A. "
 
     def do_it(self):
+        """A simple method do demonstrate the call of this function. """
         print("method of Class C is called")
 
 
-class MY_D(MY_B, MY_C):
+class MyD(MyB, MyC):
     "A class D that inherits both Classes B and C. Multiple Inheritance. "
     # pass
 
 
 def main():
-    myobject = MY_D()
+    """A main function to create objects of the above classes."""
+    myobject = MyD(2,2)
     myobject.do_it()
 
     # Multiple inheritance with old-style classes is governed by two rules:
     # depth-first and then left-to-right.
     # The order is driven using a set of rules called Method Resolution Order (MRO).
-    print(MY_D.mro())
+    print(MyD.mro())
 
 
 if __name__ == "__main__":
