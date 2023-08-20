d6d1b4880184aadd21fd1f93027ac2dc21479f8f
Kia Teymourian <kiat@cs.utexas.edu>
2023-08-19 20:13:32 -0500

    .

--- example_004_00_decorators_in_python.py
+++ example_004_00_decorators_in_python.py
@@ -1,37 +1,45 @@
+""" A simple Example for a program timing problem."""
+
 from time import time
 
-# 1. Decorator in python 
-# 2. Decorator Pattern in OOP 
+# 1. Decorator in python
+# 2. Decorator Pattern in OOP
 
-def add_numbers(a , b):
-    return a + b
+def add_numbers(my_a, my_b):
+    """A simple function to add two numbers. """
+    return my_a + my_b
 
-def multi(a, b):
-    return a * b 
+
+def multi(my_a, my_b):
+    """A simple function to multiply. """
+    return my_a * my_b
+
 
 def main():
-    a = 12
-    b = 20 
+    """A main function to demonstrate it."""
+    my_a = 12
+    my_b = 20
 
-# Time it! 
+    # Start of timing.
     before = time()
-    
-    c = add_numbers(a , b)
-    
-    after = time() 
 
-    elapsed_time = after - before 
-    print("Elapsed time is: " ,  f'{elapsed_time:.10f}' , "Miliseconds")
-
-    # Another computation  
-    before = time()
-    
-    c = multi(a, b)
+    my_c = add_numbers(my_a, my_b)
 
     after = time()
-    elapsed_time = after - before 
 
-    print("Elapsed time is: " ,  f'{elapsed_time:.10f}' , "Miliseconds")
+    elapsed_time = after - before
+    print("Elapsed time is: ",  f'{elapsed_time:.10f}', "Milliseconds")
+
+    # Another computation
+    before = time()
+
+    my_c = multi(my_a, my_b)
+
+    after = time()
+    elapsed_time = after - before
+
+    print("Elapsed time is: ",  f'{elapsed_time:.10f}', "Milliseconds")
+    print(my_c)
 
 
 if __name__ == "__main__":
